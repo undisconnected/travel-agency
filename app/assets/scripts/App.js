6306237fb30c3f54fc2e7be3f76c0745557675b9
Matt <matt@undisocnnected.com>
2018-08-19 13:22:11 +0100

    modal work done :)

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader"
 import $ from "jquery";
+import Modal from "./modules/Modal";
 
 var mobileMenu = new MobileMenu();
 
@@ -10,3 +11,4 @@ new RevealOnScroll($(".testimonial"), "60%");
 
 
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
